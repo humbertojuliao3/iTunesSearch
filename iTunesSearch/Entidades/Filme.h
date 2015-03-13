716dcad286f53e918ae8a6ff7fa45613d2440595
Humberto Luiz Julião Brandão <humbertojuliao3@gmail.com>
2015-03-13 09:57:58 -0300

    teste

--- iTunesSearch/Entidades/Filme.h
+++ iTunesSearch/Entidades/Filme.h
@@ -11,11 +11,11 @@
 @interface Filme : NSObject
 
 @property (nonatomic, strong) NSString *nome;
-@property (nonatomic, strong) NSString *tipo;
 @property (nonatomic, strong) NSString *trackId;
 @property (nonatomic, strong) NSString *artista;
 @property (nonatomic, strong) NSString *duracao;
 @property (nonatomic, strong) NSString *genero;
 @property (nonatomic, strong) NSString *pais;
+@property (nonatomic, strong) NSString *tipo;
 
 @end
